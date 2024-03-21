e4029e2b6e77e31aac5e1795a8bc2dec952b1b96
Paul Heinrich <pheinrich11235@gmail.com>
2024-03-21 13:59:19 +0100

    remove find route tracing

--- src/simulation/replanning/replanner.rs
+++ src/simulation/replanning/replanner.rs
@@ -222,7 +222,7 @@ impl ReRouteTripReplanner {
         );
     }
 
-    #[tracing::instrument(level = "trace", skip(self, from_act, to_act, veh_type_id))]
+    // #[tracing::instrument(level = "trace", skip(self, from_act, to_act, veh_type_id))]
     fn find_route(
         &self,
         from_act: &Activity,
